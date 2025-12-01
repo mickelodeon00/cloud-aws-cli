6b7eee1811d57fc9c259f25a7256f6b452ffce57
Micheal <michealakingbade00@gmail.com>
2025-12-01 22:05:04 +0100

    FE BE setup

---
+++ backend-setup.sh
@@ -0,0 +1,9 @@
+
+# ===========================
+# Stating up python 3 and java 17
+# ===========================
+sudo yum install python3 -y
+sudo yum install java-17-amazon-corretto -y
+
+
+
--- frontend-setup.sh
+++ frontend-setup.sh
@@ -2,7 +2,6 @@
 # ===========================
 # Stating up httpd web server
 # ===========================
-#!/bin/bash
 sudo yum install httpd -y
 sudo systemctl enable httpd
 sudo systemctl start httpd
